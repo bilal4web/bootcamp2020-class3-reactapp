efdd0ee81daa5b612048e3fcd7e0a7e05c5b1274
Bilal Ahmad <bilal.dourani@outlook.com>
2022-11-27 20:26:02 +0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Room() {
+  let [islet,setlet]=useState(true);
+  let [temp,setTemp]=useState(22);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const ToogleLight=()=>{
+      setlet(!islet);
+
+    }
+
+    return(
+
+        <div className={`room ${islet ? "lit": "dark"}`}>
+          <p> The Room is {islet ? "Lit": "Dark"}</p>
+          <p>{islet?"Wake up its Morning": "Go to bed Its sleep time"}</p>
+
+
+          <p>Temperature:{temp}</p>
+         
+            <br></br>
+            <button onClick={ToogleLight}>Toggle Light</button>
+
+            <br></br>
+
+            <button onClick={()=>{
+              setlet(!islet)      
+            }}>Sleep/Wakeup</button>
+
+
+            <br></br>
+
+            <button onClick={()=>{
+              setlet(true)
+                
+                }}>Turn On</button>
+            <button onClick={()=>{
+              setlet(false)
+                }}>Turn Off</button>
+
+                <br></br>
+
+                <button onClick={()=>setTemp(++temp)}>Increase Temp</button>
+                <button onClick={()=>setTemp(--temp)}>Decrease Temp</button>
+
+
+            
+        </div>
+
+        
+
+    );
+}
+
+ReactDOM.render(<Room />, document.getElementById('root'));
